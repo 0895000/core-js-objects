d0cf995be66c9d90517c993a3d009bdeef886d0a
Andrey Duka <70843050+0895000@users.noreply.github.com>
2025-02-22 17:43:33 +0300

    feat: add solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -52,8 +52,14 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = { ...obj };
+  keys.forEach((key) => {
+    if (key in result) {
+      delete result[key];
+    }
+  });
+  return result;
 }
 
 /**
